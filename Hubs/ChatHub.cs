8a0ec6de54c1f7935c97ee936249f41ed44dccde
bvalentinaa <barnavalentina98@yahoo.com>
2020-12-06 22:01:43 +0200

    auth auth condition on chat lab9

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -3,14 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Barna_Valentina_Lab2.Hubs
 {
     public class ChatHub : Hub
     {
+        [Authorize]
         public async Task SendMessage(string user, string message)
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Clients.All.SendAsync("ReceiveMessage", Context.User.Identity.Name, message);
         }
     }
 }
